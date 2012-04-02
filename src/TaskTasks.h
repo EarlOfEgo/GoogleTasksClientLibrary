d66d6a32693499ba660c4d547d553ce2edd08194
EarlOfEgo <stephan.hagios@gmail.com>
2012-04-02 17:12:24 +0200

    added comments in task file

--- src/TaskTasks.h
+++ src/TaskTasks.h
@@ -16,39 +16,39 @@
 
 typedef struct
 {
-    char *type;
-    char *description;
-    char *link;
+    char *type;         /*Type of the link, e.g. "email".*/
+    char *description;  /*The description. In HTML speak: Everything between <a> and </a>.*/
+    char *link;         /*The URL.*/
 }TaskLinks;
 
 typedef struct
 {
-    char *kind;
-    char *id;
-    char *etag;
-    char *title;
-    char *updated;
-    char *selfLink;
-    char *parent;
-    char *position;
-    char *notes;
-    char *status;
-    char *due;
-    char *completed;
-    char *deleted;
-    char *hidden;
-    int numberLinks;
-    TaskLinks *links;
+    char *kind;         /*Type of the resource. This is always "tasks#task".*/
+    char *id;           /*Task identifier.*/
+    char *etag;         /*ETag of the resource.*/
+    char *title;        /*Title of the task.*/
+    char *updated;      /*Last modification time of the task (as a RFC 3339 timestamp).*/
+    char *selfLink;     /*URL pointing to this task. Used to retrieve, update, or delete this task.*/
+    char *parent;       /*Parent task identifier. This field is omitted if it is a top-level task. This field is read-only. Use the "move" method to move the task under a different parent or to the top level.*/
+    char *position;     /*String indicating the position of the task among its sibling tasks under the same parent task or at the top level. If this string is greater than another task's corresponding position string according to lexicographical ordering, the task is positioned after the other task under the same parent task (or at the top level). This field is read-only. Use the "move" method to move the task to another position.*/
+    char *notes;        /*Notes describing the task. Optional.*/
+    char *status;       /*Status of the task. This is either "needsAction" or "completed".*/
+    char *due;          /*Due date of the task (as a RFC 3339 timestamp). Optional.*/
+    char *completed;    /*Completion date of the task (as a RFC 3339 timestamp). This field is omitted if the task has not been completed.*/
+    char *deleted;      /*Flag indicating whether the task has been deleted. The default if False.*/
+    char *hidden;       /*Flag indicating whether the task is hidden. This is the case if the task had been marked completed when the task list was last cleared. The default is False. This field is read-only.*/
+    int numberLinks;    /*Number of links*/
+    TaskLinks *links;   /*Collection of links. This collection is read-only.*/
     
 }TaskItem;
 
 typedef struct
 {
-    char *kind;
-    char *etag;
-    char *nextPageToken;
-    int numberItems;
-    TaskItem *items;
+    char *kind;         /*Type of the resource. This is always "tasks#tasks".*/            
+    char *etag;         /*ETag of the resource.*/         
+    char *nextPageToken;/*Token used to access the next page of this result.*/        
+    int numberItems;    /*Number of items*/ 
+    TaskItem *items;    /*Collection of tasks.*/
 }TaskTasks;
 
 
